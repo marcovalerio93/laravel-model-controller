469e443a9b64bb3165f6a13859048d42808a1ddc
Marco Valerio <mo.valerio93@gmal.com>
2022-10-05 11:37:43 +0200

    model movie created

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function list(){
+        $movies = Movie::all();
+
+        foreach($movies as $movie);{
+            echo $movie->title .'<br>';
+
+        }
+
+        die();
+
+    
+
+
         return view('listMovie');
     }
 }
